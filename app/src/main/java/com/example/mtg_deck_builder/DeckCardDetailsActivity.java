d8b05598ba8e825e46b5c6aefdae9478bc7bc329
Daan <dws.vanbragt@student.avans.nl>
2024-03-26 14:56:57 +0100

    finished off front end for card details

--- app/src/main/java/com/example/mtg_deck_builder/DeckCardDetailsActivity.java
+++ app/src/main/java/com/example/mtg_deck_builder/DeckCardDetailsActivity.java
@@ -4,14 +4,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mtg_deck_builder.models.DeckCard;
+import com.squareup.picasso.Picasso;
 
 public class DeckCardDetailsActivity extends AppCompatActivity {
 
+    private ImageView cardImage;
     private TextView txtCardName;
     private TextView txtCardTypeLine;
     private TextView txtCardOracleText;
@@ -19,7 +22,7 @@ public class DeckCardDetailsActivity extends AppCompatActivity {
     private TextView txtCardCmc;
     private TextView txtCardPower;
     private TextView txtCardToughness;
-    private TextView txtDeckAmount;
+    private TextView txtLegaties;
     private EditText edtComment;
     private EditText edtAmount;
     private Button btnSave;
@@ -32,6 +35,7 @@ public class DeckCardDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_deck_card_details);
 
         // Initialize views
+        cardImage = findViewById(R.id.card_image);
         txtCardName = findViewById(R.id.txtCardName);
         txtCardTypeLine = findViewById(R.id.txtCardTypeLine);
         txtCardOracleText = findViewById(R.id.txtCardOracleText);
@@ -39,7 +43,7 @@ public class DeckCardDetailsActivity extends AppCompatActivity {
         txtCardCmc = findViewById(R.id.txtCardCmc);
         txtCardPower = findViewById(R.id.txtCardPower);
         txtCardToughness = findViewById(R.id.txtCardToughness);
-        txtDeckAmount = findViewById(R.id.txtDeckAmount);
+        txtLegaties = findViewById(R.id.txtLegaties);
         edtComment = findViewById(R.id.edtComment);
         edtAmount = findViewById(R.id.edtAmount);
         btnSave = findViewById(R.id.btnSave);
@@ -69,17 +73,47 @@ public class DeckCardDetailsActivity extends AppCompatActivity {
 
     private void displayDeckCardDetails() {
         // Display card details
+        Picasso.get().load(deckCard.getCard().getImages().getNormal()).into(cardImage);
         txtCardName.setText(deckCard.getCard().getName());
-        txtCardTypeLine.setText(deckCard.getCard().getTypeLine());
-        txtCardOracleText.setText(deckCard.getCard().getOracleText());
-        txtCardManaCost.setText(deckCard.getCard().getManaCost());
-        txtCardCmc.setText(String.valueOf(deckCard.getCard().getCmc()));
-        txtCardPower.setText(deckCard.getCard().getPower());
-        txtCardToughness.setText(deckCard.getCard().getToughness());
+        txtCardTypeLine.setText("Type: " + deckCard.getCard().getTypeLine());
+        txtCardOracleText.setText("Oracle Text: " + deckCard.getCard().getOracleText());
+        txtCardManaCost.setText("ManaCost: " + deckCard.getCard().getManaCost());
+        txtCardCmc.setText(String.valueOf("CMC: " + deckCard.getCard().getCmc()));
+        txtCardPower.setText("CardP ower: " + deckCard.getCard().getPower());
+        txtCardToughness.setText("Toughness: " + deckCard.getCard().getToughness());
+        txtLegaties.setText(buildLegalities());
 
         // Display deck card details
-        txtDeckAmount.setText(String.valueOf(deckCard.getAmount()));
         edtComment.setText(deckCard.getComment());
         edtAmount.setText(String.valueOf(deckCard.getAmount()));
+    }
+
+    private String buildLegalities(){
+        StringBuilder theLaw = new StringBuilder();
+
+        theLaw.append("Standard: ").append(deckCard.getCard().getLegalities().getStandard()).append("\n");
+        theLaw.append("Future: ").append(deckCard.getCard().getLegalities().getFuture()).append("\n");
+        theLaw.append("Historic: ").append(deckCard.getCard().getLegalities().getHistoric()).append("\n");
+        theLaw.append("Timeless: ").append(deckCard.getCard().getLegalities().getTimeless()).append("\n");
+        theLaw.append("Gladiator: ").append(deckCard.getCard().getLegalities().getGladiator()).append("\n");
+        theLaw.append("Pioneer: ").append(deckCard.getCard().getLegalities().getPioneer()).append("\n");
+        theLaw.append("Explorer: ").append(deckCard.getCard().getLegalities().getExplorer()).append("\n");
+        theLaw.append("Modern: ").append(deckCard.getCard().getLegalities().getModern()).append("\n");
+        theLaw.append("Legacy: ").append(deckCard.getCard().getLegalities().getLegacy()).append("\n");
+        theLaw.append("Pauper: ").append(deckCard.getCard().getLegalities().getPauper()).append("\n");
+        theLaw.append("Vintage: ").append(deckCard.getCard().getLegalities().getVintage()).append("\n");
+        theLaw.append("Penny: ").append(deckCard.getCard().getLegalities().getPenny()).append("\n");
+        theLaw.append("Commander: ").append(deckCard.getCard().getLegalities().getCommander()).append("\n");
+        theLaw.append("Oathbreaker: ").append(deckCard.getCard().getLegalities().getOathbreaker()).append("\n");
+        theLaw.append("Standard Brawl: ").append(deckCard.getCard().getLegalities().getStandardbrawl()).append("\n");
+        theLaw.append("Brawl: ").append(deckCard.getCard().getLegalities().getBrawl()).append("\n");
+        theLaw.append("Alchemy: ").append(deckCard.getCard().getLegalities().getAlchemy()).append("\n");
+        theLaw.append("Pauper Commander: ").append(deckCard.getCard().getLegalities().getPaupercommander()).append("\n");
+        theLaw.append("Duel: ").append(deckCard.getCard().getLegalities().getDuel()).append("\n");
+        theLaw.append("Old School: ").append(deckCard.getCard().getLegalities().getOldschool()).append("\n");
+        theLaw.append("Premodern: ").append(deckCard.getCard().getLegalities().getPremodern()).append("\n");
+        theLaw.append("Predh: ").append(deckCard.getCard().getLegalities().getPredh()).append("\n");
+
+        return theLaw.toString();
     }
 }
