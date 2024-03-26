60c961f8044d54e8238b1b29d25bafd11b89210c
jgazegers <jga.zegers@student.avans.nl>
2024-03-26 22:01:25 +0100

    Add ability to store and retrieve decks from UserDefaults

--- app/src/main/java/com/example/mtg_deck_builder/models/Deck.java
+++ app/src/main/java/com/example/mtg_deck_builder/models/Deck.java
@@ -1,10 +1,18 @@
 package com.example.mtg_deck_builder.models;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.Serializable;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class Deck implements Serializable {
+    private static final String PREF_DECKS = "pref_decks";
     private UUID id;
     private String name;
     private List<DeckCard> cards;
@@ -41,12 +49,20 @@ public class Deck implements Serializable {
         }
     }
 
-    public static void saveList(List<Deck> decks) {
-        // Your implementation to save decks
+    public static void saveList(List<Deck> decks, Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_DECKS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(decks);
+        editor.putString(PREF_DECKS, json);
+        editor.apply();
     }
 
-    public static List<Deck> getListFromUserDefaults() {
-        // Your implementation to retrieve decks from UserDefaults
-        return new ArrayList<>();
+    public static List<Deck> getListFromUserDefaults(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_DECKS, Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString(PREF_DECKS, null);
+        Type type = new TypeToken<ArrayList<Deck>>() {}.getType();
+        return gson.fromJson(json, type);
     }
 }
