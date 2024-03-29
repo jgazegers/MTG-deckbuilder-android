4e5a7dfd8d6a90b7e00453937f0513114f916bf8
jgazegers <jga.zegers@student.avans.nl>
2024-03-29 10:55:21 +0100

    Add basic settings screen

--- app/src/main/java/com/example/mtg_deck_builder/MainActivity.java
+++ app/src/main/java/com/example/mtg_deck_builder/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.mtg_deck_builder;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import com.example.mtg_deck_builder.decks.DecksActivity;
 import com.example.mtg_deck_builder.search.SearchActivity;
@@ -16,9 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         Button btnSearch = findViewById(R.id.btnSearch);
         Button btnDecks = findViewById(R.id.btnDecks);
+        Button btnSettings = findViewById(R.id.btnSettings);
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, DecksActivity.class));
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
             }
         });
     }
